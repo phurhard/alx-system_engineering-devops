53a13a4cc2a6a7290ed8abf10d9f4786428c2f69
phurhard <phurhardeen@gmail.com>
2023-05-03 10:59:26 +0100

    Fixed some checks ot passsing puppet-lint

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,8 @@
 #This manifests creates a file in /tmp
 file { '/tmp/school':
-  ensure => file,
-  mode => '0744',
-  owner => 'www-data',
-  group => 'www-data',
+  ensure  => file,
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
   content => 'I love Puppet',
 }
