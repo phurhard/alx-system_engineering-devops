f807ebf686700ed2eb2e3b0e25dff63d0f20eee3
phurhard <phurhardeen@gmail.com>
2023-05-23 19:35:53 +0100

    pycodestyle validated

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -14,7 +14,7 @@ if __name__ == "__main__":
         users = user.json()
         todo = requests.get(todoURL)
         todo_json = todo.json()
-        user_dict={}
+        user_dict = {}
         for user in users:
             username = user.get("username")
             user_id = user.get("id")
@@ -26,29 +26,8 @@ if __name__ == "__main__":
                         "task": todos.get("title"),
                         "completed": todos.get("completed")
                         })
-            print(f'{user_id}: {lst}')
             user_dict[f'{user_id}'] = lst
-
 
         file = "todo_all_employees.json"
         with open(file, 'w') as wr:
             wr.write(json.dumps(user_dict))
-    '''userdict = {}
-    userlist = []
-    lst = []
-    for item in todo_json:
-        if item.get("userId") == userId:
-            userlist.append(item)
-    for item in userlist:
-        lst.append({
-            "task": item["title"],
-            "completed": item["completed"],
-            "username": username
-            })
-    # print(lst)
-    userdict[f'{userId}'] = lst
-    # print(userdict)
-    file = f'{userId}' + '.json'
-    with open(file, 'w') as wr:
-        wr.write(json.dumps(userdict))'''
-
