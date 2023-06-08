cc15924dd2ff4b5af8f7f65b1dc2213e2b01a659
phurhard <phurhardeen@gmail.com>
2023-06-08 09:32:19 +0100

    again

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,27 +1,27 @@
 #!/usr/bin/python
 """ A module for sending requests to Reddit API endpoints
 """
-
 import requests
 
 
 def recurse(subreddit, hot_list=[]):
-    """ Uses recursion to print out all the titles in the hot section for a given subreddit
+    """ Uses recursion to print out all the
+    titles in the hot section for a given subreddit
     """
-    
     headers = {"User-Agent": "phurhard"}
 
     try:
         if not hot_list:
-            url = "https://www.reddit.com/r/{}/hot.json?limit=25".format(subreddit)
+            url = "https://www.reddit.com/r/{}/hot.json?\
+            limit=25".format(subreddit)
         else:
             last_post = hot_list[-1]
             after = last_post['data']['name']
-            url = "https://ww.reddit.com/r/{}/hot.json?limit=25&after={}".format(subreddit, after)
+            url = "https://ww.reddit.com/r/{}/hot.json?\
+            limit=25&after={}".format(subreddit, after)
         res = requests.get(url, headers=headers)
         data = res.json()
         posts = data['data']['children']
-        
         for post in posts:
             hot_list.append(post['data']['title'])
         after = data['data']['after']
