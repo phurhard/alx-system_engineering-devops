f6e1b58cce717416d7ad9a08e8f198c61bd0c657
phurhard <phurhardeen@gmail.com>
2023-06-15 17:54:30 +0100

    nginx

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,20 @@
+$file_path = '/etc/default/nginx'  # Specify the path to the file you want to modify
+
+# Backup the original file
+file { "${file_path}.bak":
+  ensure => 'file',
+  source => $file_path,
+}
+
+# Replace the line with the desired value
+file { $file_path:
+  ensure  => 'file',
+  replace => 'true',
+  content => 'ULIMIT="-n 4096"',
+  notify  => Exec['reload_service'],
+}
+
+exec { 'reload_service':
+  command     => 'service your_service_name restart',
+  refreshonly => true,
+}
---
+++ 0x1B-web_stack_debugging_4/README.md
@@ -0,0 +1,1 @@
+WEb stack debugging is really tasking, solutions are mostly things you don't even think about
